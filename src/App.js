977f950c3a1aeb89a6dfd60869550ab846146606
Amandine LEFEBVRE <57723313+amandine-dev@users.noreply.github.com>
2020-12-17 11:48:36 +0100

    changement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Canvas } from "react-three-fiber";
+import { OrbitControls, Sky } from "drei"
+import './App.css'
 
-function App() {
+function Box (){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <mesh>
+      <boxBufferGeometry attach = "geometry"/>
+      <meshLambertMaterial attach="material" color="yellow"/>
+    </mesh>
+  )
 }
 
-export default App;
+
+
+export default function App(){
+  return <Canvas>
+    <OrbitControls/>
+    <Sky />
+
+    <ambientLight intensity={0.5}/>
+    <spotLight position ={[10, 15, 10]} angle={0.3}/>
+    <Box/>
+  </Canvas>
+};
